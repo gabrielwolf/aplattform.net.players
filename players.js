d89738cae0275e9a62fb427aa757b2c0539bf227
Gabriel Wolf <gabriel.wolf@wolfzeit.com>
2020-07-29 17:47:37 +0200

    Fix: user gesture policy 'The AudioContext was not allowed to start...'

--- players.js
+++ players.js
@@ -1,19 +1,24 @@
 'use strict'
 
 class Player {
-  constructor (src) {
+  constructor (src, order) {
     this.src = src
+    this.order = order
   }
 
   src = () => this.src
+  order = () => this.order
   play = () => console.log('Player playing...')
 }
 
 class FOAPlayer extends Player {
-  constructor (src) {
-    super(src)
+  constructor (src, order) {
+    super(src, order)
+  }
+
+  initialize = () => {
     this.audioElement = document.createElement('audio')
-    this.audioElement.src = src
+    this.audioElement.src = this.src
     this.audioElement.loop = false
     this.audioElement.crossOrigin = 'anonymous'
     this.audioContext = new AudioContext()
@@ -38,24 +43,29 @@ class FOAPlayer extends Player {
       console.log('FOAPlayer playing...')
     })
   }
+
   pause = () => this.audioElement.pause()
 }
 
 class HOAPlayer extends Player {
   constructor (src, order) {
-    super(src)
+    super(src, order)
+  }
+
+  play = () => {
     this.audioContext = new AudioContext()
     this.inputGain = this.audioContext.createGain()
     this.hoaRenderer = Omnitone.createHOARenderer(this.audioContext,
-      { ambisonicOrder: order })
+      { ambisonicOrder: this.order })
     let moreChannels
-    if (order === 2)
+    if (this.order === 2)
       moreChannels = '_ch9'
-    else if (order === 3)
+    else if (this.order === 3)
       moreChannels = '_ch9-16'
-    const file1 = src.substring(0, src.length - 5) + '_ch1-8' + src.slice(-5)
-    const file2 = src.substring(0, src.length - 5) + moreChannels +
-      src.slice(-5)
+    const file1 = this.src.substring(0, this.src.length - 5) + '_ch1-8' +
+      this.src.slice(-5)
+    const file2 = this.src.substring(0, this.src.length - 5) + moreChannels +
+      this.src.slice(-5)
     Promise.all([
       Omnitone.createBufferList(this.audioContext, [file1, file2]),
       this.hoaRenderer.initialize(),
@@ -64,19 +74,18 @@ class HOAPlayer extends Player {
         results[0])
       this.inputGain.connect(this.hoaRenderer.input)
       this.hoaRenderer.output.connect(this.audioContext.destination)
+      this.currentBufferSource = this.audioContext.createBufferSource()
+      this.currentBufferSource.buffer = this.contentBuffer
+      this.currentBufferSource.loop = false
+      this.currentBufferSource.connect(this.inputGain)
+      this.currentBufferSource.start()
+      console.log('HOAPlayer playing...')
     })
   }
 
-  play = () => {
-    this.currentBufferSource = this.audioContext.createBufferSource()
-    this.currentBufferSource.buffer = this.contentBuffer
-    this.currentBufferSource.loop = false
-    this.currentBufferSource.connect(this.inputGain)
-    this.currentBufferSource.start()
-    console.log('HOAPlayer playing...')
-  }
   stop = () => {
     this.currentBufferSource.stop()
     this.currentBufferSource.disconnect()
   }
 }
+
