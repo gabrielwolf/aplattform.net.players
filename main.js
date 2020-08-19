072f5130de574d063b1f8531671f74f61b7565e7
Gabriel Wolf <gabriel.wolf@wolfzeit.com>
2020-08-19 17:46:48 +0200

    UX: display duration

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 import OmnitonePlayer from './modules/omnitonePlayer.js'
 
+function secondsToReadableTime (seconds) {
+  let time = new Date(null)
+  time.setSeconds(seconds)
+  time = time.toISOString()
+  time = (seconds > 3600)
+    ? time.substr(11, 8)
+    : time.substr(14, 5)
+  time = (time.substr(0, 1) == 0) ? time.substring(1) : time
+  return time
+}
+
 const e = {
   // First order FuMa example
   foa: window.foa = new OmnitonePlayer('sounds/foa.flac', 1,
@@ -34,6 +45,10 @@ for (const eKey in e) {
         document.getElementById(eKey + '-azimuth').disabled = false
         document.getElementById(eKey + '-elevation').disabled = false
         document.getElementById(eKey + '-load').innerText = 'Loaded'
+        document.getElementById(
+          eKey + '-time').innerText = secondsToReadableTime(
+          e[eKey].durationInSeconds,
+        )
       })
     })
   document.getElementById(eKey + '-play').
