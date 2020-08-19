86093f8cba2aecfae53c4abee2cb4b5754f91fee
Gabriel Wolf <gabriel.wolf@wolfzeit.com>
2020-08-19 19:59:10 +0200

    Fix: type error

--- main.js
+++ main.js
@@ -112,6 +112,7 @@ for (const eKey in e) {
         textContent = String(elevation)
       e[eKey].rotateSoundfield(azimuth, elevation)
     })
+
 }
 
 function secondsToReadableTime (seconds) {
@@ -121,6 +122,6 @@ function secondsToReadableTime (seconds) {
   time = (seconds > 3600)
     ? time.substr(11, 8)
     : time.substr(14, 5)
-  time = (time.substr(0, 1) === 0) ? time.substring(1) : time
-  return time
+  time = (time.substr(0, 1) === '0') ? time.substring(1) : time
+  return String(time)
 }
